066fec08b4379c97af05fde6ef39694cc8bffd06
Nicole Santamaria and Tyler Stephenson <nicole@>
2017-03-30 10:55:49 -0700

    in scripts.js two while loops review userInput and return roman numeral up to X.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,37 @@
 //business logic
 var romanNumeral = function(userInput){
-  
-}
+  var romanNumeralArray = ["I", "V", "X", "L", "C", "D", "M"];
+  var romanNumeralSplit = [];
+  var text = "";
+  var i = 0;
+  while (i < 5 && i < userInput) {
+    if (i<3) {
+      text += "I";
+      i++;
+    } else if (i===3) {
+      text = "IV";
+      i++;
+    } else if (i===4) {
+      text = "V";
+      i++;
+    }
+  };
+  while (i <= 9 && i < userInput) {
+    if (i<8) {
+      text += "I";
+      i++;
+    } else if (i===8) {
+      text = "IX";
+      i++;
+    } else if (i===9) {
+      text = "X";
+      i++;
+    }
+  };
+  romanNumeralSplit.push(text);
+  return romanNumeralSplit.join('');
+};
+
 
 //user interface logic
 $(document).ready(function(){
